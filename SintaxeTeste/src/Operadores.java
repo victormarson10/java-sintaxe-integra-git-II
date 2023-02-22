7f695c0fce5eba00c324f1af19154d09fe2f2e4d
victor10 <victormarson10@gmail.com>
2023-02-22 12:20:50 -0300

    adicionando nova class via vscode

---
+++ SintaxeTeste/src/Operadores.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Operadores {
+    public static void main(String[] args) {
+
+        int primeironumero;
+        int segundonumero;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro valor");
+        primeironumero = scanner.nextInt();
+        System.out.println(primeironumero);
+
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Digite o segundo valor");
+        segundonumero = scanner1.nextInt();
+        System.out.println(segundonumero);
+
+        String simNaoResult;
+        boolean simNao = true;
+
+        simNao = primeironumero == segundonumero;
+
+        /*String simNaoResult = (simNao = false ? "Falso" : "Verdadeiro");*/
+
+        if (simNao) {
+            simNaoResult = "O número " + primeironumero + " é igual ao " + segundonumero ;
+        } else {
+            simNaoResult = "O número " + primeironumero + " não é igual ao " + segundonumero;
+        }
+
+        System.out.println("Número " + primeironumero + " é igual ao número " + segundonumero + "? " + simNaoResult);
+    }
+}
